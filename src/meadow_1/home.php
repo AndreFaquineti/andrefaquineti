af08041bba9961009a9d4bda8af4a6f92fc21a38
AndreFaquineti <142813791+AndreFaquineti@users.noreply.github.com>
2026-01-15 13:23:25 -0300

    2026.01.15 - 1
    Frontend updates. Responsiveness and button sizes

--- src/meadow_1/home.php
+++ src/meadow_1/home.php
@@ -13,11 +13,22 @@ not_logged();
 </head>
 <body>
 <!--HEADER START-->
-<div style="display: flex; justify-content: end;">
+<div id="header-home" class="flex-container-direction-row" style="display: flex; justify-content: end;">
+    <div id="header-nameDisplay">
+        <p>
+            <?php echo "Logged as: " . $_SESSION["user_nickname"] . "<br>Under " . $_SESSION["user_email"]?>
+        </p>
+    </div>
+    <a href="settings.php">
+        <img
+          src="images/settings-icon.svg"
+          class="control-button2"
+          id="submitInput">
+    </a>
     <a href="exit.php">
         <img
           src="images/logout-icon.svg"
-          class="control-button"
+          class="control-button2"
           id="submitInput">
     </a>
 </div>
@@ -62,9 +73,61 @@ style="text-align: center; justify-content: center; flex-wrap: wrap;"
 <div id="dbstate"></div>
 </body>
 </html>
-<script src="sistema/timer.js"></script>
 <script>
-    fetch("sistema/conexao.php")
+let startTime;
+let stopTime;
+let elapsedTime;
+let elapsedSeconds;
+let myTimer;
+let timerStarted;
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+
+startButton.addEventListener("click", startWatch);
+function startWatch() {
+  if (timerStarted != true) {
+    timerStarted = true;
+    startTime = new Date();
+    console.log("start time = ", startTime);
+    myTimer = setInterval(updateDisplay, 1000);
+    
+    startButton.style.filter = "brightness(100%)";
+  }
+  console.log("startButton Clicked!");
+}
+
+function stopWatch() {
+  console.log("stopButton Clicked!");
+  totalSeconds = -1;
+  updateDisplay();
+  stopTime = new Date();
+  timerStarted = false;
+  clearInterval(myTimer);
+
+  startButton.style.filter = "";
+}
+stopButton.addEventListener("click", stopWatch);
+
+
+/*TIMER FRONTEND START*/
+var totalSeconds = 0;
+var totalMinutes = 0;
+var totalHours = 0;
+const displayWatch = document.getElementById("displayWatch");
+
+function formatTime() {
+  const s = String(totalSeconds % 60).padStart(2, "0");
+  const m = String((Math.floor(totalSeconds / 60) % 60)).padStart(2, "0");
+  const h = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
+  return `${h}:${m}:${s}`;
+}
+function updateDisplay() {
+  totalSeconds = totalSeconds + 1;
+  displayWatch.textContent = formatTime();
+}
+/*TIMER FRONTEND END*/
+
+    fetch("sistema/sys_timer.php")
     .then(promise => promise.text())
     .then(resultado =>{
         document.getElementById("dbstate").innerHTML = resultado;
