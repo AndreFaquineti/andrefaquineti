5f4b264402b610d603564c3ceee862a0875c0f8a
AndreFaquineti <142813791+AndreFaquineti@users.noreply.github.com>
2026-03-04 21:11:27 -0300

    2026.04.03 - html pathing test

---
+++ src/meadow/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meadow - Home</title>
+    <meta charset="UT-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+<stopwatch>
+    <img id="swImage" src="/andrefaquineti/src/meadow/images/timer-icon.svg" class="icon1">
+    <div id="swDisplay"></div>
+    <div id="swControllers">
+        <img id="swImage" src="/andrefaquineti/src/meadow/images/play-icon.svg" class="controlButton">
+        <img id="swImage" src="/andrefaquineti/src/meadow/images/stop-icon.svg" class="controlButton">
+    </div>
+    <div id="swTag"></div>
+    <div id="swSubtag"></div>
+</stopwatch>
+</body>
+</html>
+<script>
+    const swDisplay = document.getElementById("")
+</script>
