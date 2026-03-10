c703d877abfb0467e9c03e20d9b4740bea0faca0
AndreFaquineti <142813791+AndreFaquineti@users.noreply.github.com>
2026-03-10 19:26:54 -0300

    Update statsController.php

--- src/meadow/controllers/statsController.php
+++ src/meadow/controllers/statsController.php
@@ -6,7 +6,8 @@ if (isset($_GET['request'])) {
 }
 if (isset($request) && $request == "getSessions") {
     $getSessions = $connection->prepare(query:
-            "SELECT tag, subtag, start_time, duration_seconds FROM sessions WHERE id_user=:id_user;"
+            "SELECT tag, subtag, start_time, duration_seconds FROM sessions WHERE id_user=:id_user
+            ORDER BY id_session DESC;"
         );
         $getSessions->bindParam(':id_user', $_SESSION['id_user']);
     $getSessions->execute();
